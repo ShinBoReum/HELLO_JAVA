3e9a2059d2bd39d92d9c54040272e2744ad0611b
ShinBoReum <tlsqhfma@naver.com>
2021-02-15 23:49:02 +0900

    Initial commit

---
+++ src/chapter2/ByteVariable.java
@@ -0,0 +1,8 @@
+package chapter2;
+
+public class ByteVariable {
+    public static void main(String[] args){
+
+    }
+
+}
---
+++ src/chapter2/VariableEx.java
@@ -0,0 +1,16 @@
+package chapter2;
+
+public class VariableEx {
+    public static void main(String[] args){
+        int num;
+        num = 10;
+
+        System.out.println(num);
+
+        int level = 20;
+        System.out.println(level);
+
+        long longNum = 1234567890333333L; //logn형도 기본적으로 메모리에 4바이트로 저장되기 떄문에 L을 사용하여 long 형임을 명시.
+    
+    }
+}
