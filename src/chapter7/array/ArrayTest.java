8b70034ce6c1520f2cecee69ddf07d8664bf7545
ShinBoReum <tlsqhfma@naver.com>
2021-03-03 23:27:12 +0900

    배열

---
+++ src/chapter7/array/ArrayTest.java
@@ -0,0 +1,17 @@
+package chapter7.array;
+
+public class ArrayTest {
+    public static void main(String[] args) {
+        int [] numbers = new int[] {0,1,2}; //배열 초기화
+        int [] num = {1,2,3};  //이런식으로 초기화
+        int [] numbers2 = new int[3]; //초기화 안하면 0으로 초기화
+        numbers2[0] =1;
+        numbers2[1] =2;
+        numbers2[2] =3;
+
+        int[] nums;
+        nums = new int[] {1,2,3};
+
+
+    }
+}
